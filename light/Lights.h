10db2707527f64ac7eb3e6935dc96106d175e28c
CorentinR <coreentin@users.noreply.github.com>
2023-05-02 13:15:28 +0200

    Update Lights.h
    zeelog/android_device_xiaomi_mido lineage-19.1 --> markw

--- light/Lights.h
+++ light/Lights.h
@@ -1,44 +1,65 @@
 /*
- * Copyright (C) 2021-2022 The LineageOS Project
+ * Copyright (C) 2019 The LineageOS Project
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #pragma once
 
-#include <aidl/android/hardware/light/BnLights.h>
+#include <android/hardware/light/2.0/ILight.h>
+#include <hardware/lights.h>
+#include <hidl/Status.h>
+#include <map>
 #include <mutex>
-#include "Backlight.h"
+#include <vector>
 
-using ::aidl::android::hardware::light::HwLightState;
-using ::aidl::android::hardware::light::HwLight;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::light::V2_0::Flash;
+using ::android::hardware::light::V2_0::ILight;
+using ::android::hardware::light::V2_0::LightState;
+using ::android::hardware::light::V2_0::Status;
+using ::android::hardware::light::V2_0::Type;
 
-namespace aidl {
+typedef void (*LightStateHandler)(const LightState&);
+
+struct LightBackend {
+    Type type;
+    LightState state;
+    LightStateHandler handler;
+
+    LightBackend(Type type, LightStateHandler handler) : type(type), handler(handler) {
+        this->state.color = 0xff000000;
+    }
+};
+
 namespace android {
 namespace hardware {
 namespace light {
+namespace V2_0 {
+namespace implementation {
 
-class Lights : public BnLights {
-public:
-    Lights();
+class Light : public ILight {
+  public:
+    Return<Status> setLight(Type type, const LightState& state) override;
+    Return<void> getSupportedTypes(getSupportedTypes_cb _hidl_cb) override;
 
-    ndk::ScopedAStatus setLightState(int32_t id, const HwLightState& state) override;
-    ndk::ScopedAStatus getLights(std::vector<HwLight> *_aidl_return) override;
-private:
-    void setLED(const HwLightState& state);
-
-    std::vector<HwLight> mLights;
-
-    BacklightDevice *mBacklightDevice;
-    std::vector<std::string> mButtonsPaths;
-    bool mWhiteLED;
-
-    std::mutex mLEDMutex;
-    HwLightState mLastBatteryState;
-    HwLightState mLastNotificationState;
+  private:
+    std::mutex globalLock;
 };
 
-} // namespace light
-} // namespace hardware
-} // namespace android
-} // namespace aidl
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace light
+}  // namespace hardware
+}  // namespace android
